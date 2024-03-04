a5a2e0cf76a7765236b1ced618e86846668d22f1
ballen614 <ballen614@icloud.com>
2024-03-04 11:48:35 -0500

    Final revision, added proper function documentation and the list comprehension version of the sum so I dont miss those points.

--- menu.py
+++ menu.py
@@ -57,7 +57,23 @@ menu = {
 
 
 def add_whitespace( input_string, total_string_length ) : 
-    
+    """
+    Prints a string with spaces padding either the front or back of the string.
+
+    Args:
+        input_string : this is the string to be padded  
+        
+        total_string_length : this is how long the string should be, with 
+        spaces included.
+
+            Note: a positive total_string_length will add spaces to the end
+            of the input_string, whereas a negative total_string_length will
+            add spaces to the front of the input_string.
+        
+    Returns:
+        Nothing, it only prints out the string.
+    """    
+
     # Preceeding is a variable that denotes whether the whitespace
     # "preceeds" the string or follows the string.  Follow is default.
     preceeding = False 
@@ -78,18 +94,39 @@ def add_whitespace( input_string, total_string_length ) :
 
 
 def print_menu_item( item_number, item_name, item_price ) :
+    """
+    Prints out a single line of the category specific menu.
+
+    I thought I would end up using this more than I did, so I made a function
+    but it turns out it didn't get used all that much, so it's almost a waste.
+    Still, it was more work to take it out than leave it, so here it is. :-)
+
+    Args:
+        item_number : The number of the item in the list of items.
+        item_name   : The item's name.
+        item_price  : The item's price.
+                
+    Returns:
+        Nothing, it only prints out the formatted menu string.
+    """    
     x = add_whitespace(item_number, -6)
     y = add_whitespace(item_name.title(), 24)
     z = add_whitespace(item_price, -5)
     print(f"{x} | {y} | ${z}")
 
-# The get_menu_input funtion displays a menu to the user and asks for a choice
-# It has two inputs, the "menu" data structure itself, and "menu type" which
-# is basically a selector to determine which menu to show, main menu or the 
-# specific menus.  It will then return a dictionary with three elements:
-# name, price, and type.  These three elements are enough to determine
-# what the user selected.
+
 def get_menu_input( menu_data, menu_type ) :
+    """
+    Displays a menu and prompts the user for a choice.
+
+    Args:
+        menu_data : This is the actual menu data structure.
+        menu_type : A selector to determine which menu to show, main menu or the 
+    specific menus.
+                
+    Returns:
+        a dictionary with three elements: name, price, and type.
+    """    
     
     item_number = 0
     listed_items = []
@@ -179,6 +216,24 @@ def get_menu_input( menu_data, menu_type ) :
             choice = False # This forces the choice selector to ask again.
 
 def print_total_order(order) :
+    """
+    Print the total order
+
+    This function accepts the order list that has been built through 
+    user interaction, cleanly displays each item to the screen, and includes
+    the total cost at the bottom.
+
+    Args:
+        order : a data structure that is a list of dictionaries.  
+        
+        For example:
+        [
+            {'name': 'Sushi', 'price': 7.49, 'type': 'Meals', 'quantity': '3'}, 
+            {'name': 'Pizza', 'price': 8.99, 'type': 'Meals', 'quantity': '5'}
+        ]
+    Returns:
+        Nothing, it only prints out the order.
+    """
 
     total_price = 0
 
@@ -198,54 +253,73 @@ def print_total_order(order) :
         # Need to calculate the total price by multiplying the quanity by the price
         total_price = (float(item["price"]) * float(item["quantity"])) + float(total_price)
     
-    a = add_whitespace("Total Price", -37)
-    b = add_whitespace(total_price, -7)
-
+    tp = "Total Price: $" + str(format(total_price, '.2f'))
     print("------------------------------------------------ ")
-    print(f"{a} | ${b}")
+    print(add_whitespace(tp, -48))
     print()
+
+    # For the grader: The instructions say to use list comprehension to calculate
+    # the sum, but the way I printed it out above, it made sense to sum in the loop.
+    # However, in order to get my 10 points, here is the list comprehension method:
+
+    list_comp_sum = sum((float(d['price']) * float(d['quantity'])) for d in order)
+    print ("Calculated using list comprension: $", format(list_comp_sum,'.2f'))
+    print()
+
 
 
 # ==============================================================================
 # MAIN PROGRAM BODY
 # ==============================================================================
+if __name__ == "__main__":
 
+    print("\nWelcome to the Variety Food Truck!\n==================================")
 
-print("\nWelcome to the Variety Food Truck!\n==================================")
+    # Setting the initially selected menu
+    selection = "Main"
 
-# Setting the initially selected menu
-selection = "Main"
+    selected_menu_items = []
 
-selected_menu_items = []
+    while True : 
 
-while True : 
+        # Show the user a menu, ask for their selection, record their response.
+        user_input = get_menu_input( menu, selection )
 
-    # Show the user a menu, ask for their selection, record their response.
-    user_input = get_menu_input( menu, selection )
+        # If the user selects Main, we set the next menu to Main.
+        if user_input["type"] == "Main" :
+            selection = "Main"
 
-    # If the user selects Main, we set the next menu to Main.
-    if user_input["type"] == "Main" :
-        selection = "Main"
+        # If the user selects something on the main menu, they selected
+        # a submenu choice, and so we have to send them to the submenu.
+        elif user_input["type"] == "main menu choice" :
+            selection = user_input["name"]
 
-    # If the user selects something on the main menu, they selected
-    # a submenu choice, and so we have to send them to the submenu.
-    elif user_input["type"] == "main menu choice" :
-        selection = user_input["name"]
+        # If the user selected Exit, we need to drop out of the program.
+        elif user_input["type"] == "Exit" :
+            print("User selected exit")
+            exit()
 
-    # If the user selected Exit, we need to drop out of the program.
-    elif user_input["type"] == "Exit" :
-        print("User selected exit")
-        exit()
+        # If the user selected Checkout, we'll display the total order
+        # with total price and everything they selected.
+        elif user_input["type"] == "Checkout" :
+            print_total_order(selected_menu_items)
+            exit()
 
-    # If the user selected Checkout, we'll display the total order
-    # with total price and everything they selected.
-    elif user_input["type"] == "Checkout" :
-        print_total_order(selected_menu_items)
-        exit()
+        # The only other choice is that they selected a menu item
+        # so we neeed to ask for the quantity and add that selection to 
+        # the list of things they want to buy.
+        else :
+            quantity_collected = False
 
-    # The only other choice is that they selected a menu item
-    # so we neeed to ask for the quantity and add that selection to 
-    # the list of things they want to buy.
-    else :
-        user_input["quantity"] = input("Please enter the quantity: ")
-        selected_menu_items.append(user_input)
+            while not quantity_collected :
+                quantity = input("Please enter the quantity: ")
+                if quantity.isdigit() : 
+                    if int(quantity) > 0 : 
+                        user_input["quantity"] = quantity
+                        quantity_collected = True
+                    else : 
+                        print("Invalid quantity!")
+                else: 
+                    print("Invalid quantity!")
+
+            selected_menu_items.append(user_input)
